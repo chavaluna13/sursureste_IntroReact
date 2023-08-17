ac736cc07c7231f395746d2733b727d202891d0f
jesualujan <lujan.oficial@gmail.com>
2023-08-16 20:19:58 -0600

    creamos nuestro componente Sensei

---
+++ 02.react-props/src/Components/Sensei.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default class Sensei extends React.Component {
+    render (){
+        console.log(this.props)
+        return (
+            <div style={
+                {
+                backgroundColor: 'lightblue',
+                margin:15
+            }
+            }> 
+             <h1>Hola {this.props.name}</h1>
+             <h2>{this.props.email}</h2>
+             <h3>{this.props.ocupation}</h3>
+            </div>
+
+
+        )
+    }
+}
