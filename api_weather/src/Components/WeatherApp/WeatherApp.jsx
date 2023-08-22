6c9f0672a38dcdbe320d7d8e95426ab7d58960a6
jesualujan <lujan.oficial@gmail.com>
2023-08-21 21:25:15 -0600

    trabajamos un poco de lógica

--- api_weather/src/Components/WeatherApp/WeatherApp.jsx
+++ api_weather/src/Components/WeatherApp/WeatherApp.jsx
@@ -13,6 +13,34 @@ function WeatherApp() {
        {/* TODA NUESTRA LÓGICA  */}
 
 
+      const search = async () => {
+        const element = document.getElementsByClassName("cityInput")
+            if(element[0].value===''){
+                return 0
+            }
+
+            let URI = `https://api.openweathermap.org/data/2.5/weather?q=${element[0].value}&unit&lat=44.34&lon=10.99&appid=${process.env.REACT_APP_API_KEY}`
+      
+            {/* hago la llamada a la API */}
+           
+            let response = await fetch(URI)
+            let data = await response.json()
+
+                {/* SELECCIONAR ELEMENTO */}
+            const humidity = document.getElementsByClassName("humidity-percent")
+            const wind = document.getElementsByClassName("wind-velocity")
+            const temprature = document.getElementsByClassName("weather-temp")
+            const location = document.getElementsByClassName("weather-location")
+
+                {/* MANIPULAR ELEMENTO */}
+                humidity[0].innerHTML=data.main.humidity+"%"
+                wind[0].innerHTML=data.wind.speed+"km/h"
+                temprature[0].innerHTML=data.main.temp+"°C"
+                location[0].innerHTML=data.name
+      }
+     
+      
+
 
 
 
@@ -22,7 +50,7 @@ function WeatherApp() {
       <div className="container">
             <div className="top-bar">
                 <input type="text" className="cityInput" placeholder="Buscar" />
-                    <div className="search-icon">
+                    <div className="search-icon" onClick={()=>{search()}}>
                      <img src={search_icon}  alt="search_icon" />
                     </div>
              </div>
