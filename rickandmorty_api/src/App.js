0436ab36196b1728f6e5d6d05e846109413e5606
jesualujan <lujan.oficial@gmail.com>
2023-08-28 19:53:07 -0600

    usamos los hooks de react

--- rickandmorty_api/src/App.js
+++ rickandmorty_api/src/App.js
@@ -1,13 +1,28 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import Navbar from './Components/Navbar/Navbar.js';
 
 function App() {
   // TENEMOS NUESTRA LÓGICA
-  //hacemos la llamada al endpoint de la api 
-  fetch("https://rickandmortyapi.com/api/character")
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(error => console.log(error))
+ 
+  //LE INDICO QUE MI ESTADO ESTÁ VACIO Y QUE SE VA A LLENAR/POBLAR DE LOS DATOS QUE VIENEN DE LA API
+  const [character, setResults] = useState([])
+
+  // indico la ruta o el endpoint de la api 
+  const URI = "https://rickandmortyapi.com/api/character"
+
+  const getCharacters = (URI) => {
+    fetch(URI)
+    //esta respuesta la tenemos que convertir a un objeto de javascript
+    .then((response) => response.json())
+    .then((data) => setResults(data.results))
+    .catch((error) => console.log(error))
+  }
+
+  // CON DEPENDENCIA VACIA, LA LLAMADA SE EJECUTA UNA VEZ EN LO QUE SE CARGA MI COMPONENTE
+  // Y SE RENDERIZA.
+  useEffect(() => {
+    getCharacters(URI)//CUANDO SE CARGUE EL COMPONENTE, LLAMA AL METODO Y EJECUTA LA FUNCIÓN ANTERIOR
+  }, [])
 
 
 
