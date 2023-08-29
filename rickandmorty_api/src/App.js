d1ede9041895da551d563e6a70e91447b1a88967
jesualujan <lujan.oficial@gmail.com>
2023-08-28 21:31:00 -0600

    importamos pagination y hacemos una nueva llamada a la api

--- rickandmorty_api/src/App.js
+++ rickandmorty_api/src/App.js
@@ -1,12 +1,14 @@
 import React, {useEffect, useState} from 'react'
 import Navbar from './Components/Navbar/Navbar.js';
 import Character from './Components/Character/Character.js';
+import Pagination from './Components/Pagination/Pagination.js';
 
 function App() {
   // TENEMOS NUESTRA LÓGICA
  
   //LE INDICO QUE MI ESTADO ESTÁ VACIO Y QUE SE VA A LLENAR/POBLAR DE LOS DATOS QUE VIENEN DE LA API
   const [character, setResults] = useState([])
+  const [info, setInfo] = useState ({})
 
   // indico la ruta o el endpoint de la api 
   const URI = "https://rickandmortyapi.com/api/character"
@@ -15,9 +17,24 @@ function App() {
     fetch(URI)
     //esta respuesta la tenemos que convertir a un objeto de javascript
     .then((response) => response.json())
-    .then((data) => setResults(data.results))
+    .then((data) => {
+      setResults(data.results)
+      setInfo(data.info)
+    })
     .catch((error) => console.log(error))
   }
+
+
+// ESTÁS DOS NUEVAS FUNCIONES HARAN UNA NUEVA LLAMA A LA MISMA API
+// PERO A DIFERENTES RUTAS 
+
+const onPrevious = () => {
+  getCharacters(info.prev)
+}
+
+const onNext = () => {
+  getCharacters(info.next)
+}
 
   // CON DEPENDENCIA VACIA, LA LLAMADA SE EJECUTA UNA VEZ EN LO QUE SE CARGA MI COMPONENTE
   // Y SE RENDERIZA.
@@ -26,18 +43,16 @@ function App() {
   }, [])
 
 
-
-
-
-
   // TENEMOS NUESTROS COMPONENTES
-
   return (
     <>
      <Navbar brand="Rick and Morty"/>
 
     <div className="container">
+        {/* pasamos el array a la props */}
+      <Pagination prev={info.prev} next={info.next} onPrevious={onPrevious} onNext={onNext} />
       <Character character={character} />
+      <Pagination prev={info.prev} next={info.next} onPrevious={onPrevious} onNext={onNext} />
     </div>
 
     </>
