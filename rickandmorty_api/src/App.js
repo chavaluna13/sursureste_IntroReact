2628242c0233d26aa88ae7a6147740c826d842de
jesualujan <lujan.oficial@gmail.com>
2023-08-28 19:33:05 -0600

    importar navbar, hacer la llamada a la api

--- rickandmorty_api/src/App.js
+++ rickandmorty_api/src/App.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import Navbar from './Components/Navbar/Navbar.js';
 
 function App() {
+  // TENEMOS NUESTRA LÓGICA
+  //hacemos la llamada al endpoint de la api 
+  fetch("https://rickandmortyapi.com/api/character")
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.log(error))
+
+
+
+
+
+
+  // TENEMOS NUESTROS COMPONENTES
+
   return (
     <>
-    <h1>Todo funciona</h1>
+     <Navbar brand="Rick and Morty"/>
     </>
   );
 }
