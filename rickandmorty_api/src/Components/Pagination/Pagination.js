5955ce6f18bcb5f0e8947e1cfe8bb53561f608f4
jesualujan <lujan.oficial@gmail.com>
2023-08-28 21:30:43 -0600

    creamos el componente pagination

---
+++ rickandmorty_api/src/Components/Pagination/Pagination.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+const Pagination = ({prev, next, onPrevious, onNext}) => {
+
+    const handlePrevious = () => {
+        onPrevious()
+    }
+
+    const handleNext = () => {
+        onNext()
+    }
+
+
+
+  return (
+    <nav className="my-6">
+        <ul className="pagination justify-content-center">
+            {
+                prev ?
+                 (
+                    <li className="page-item">
+                    <button className="page-link" onClick={handlePrevious}>previous</button>
+                    </li>
+                ):null
+            }
+            {
+                next ? 
+                (
+                    <li className="page-item">
+                    <button className="page-link" onClick={handleNext}>next</button>
+                    </li>
+                ) :null
+            }
+        </ul>
+    </nav>
+  )
+}
+
+export default Pagination
