dde73c5cbfe61b0bcd7fa97bcc7bf97554e68b22
jesualujan <lujan.oficial@gmail.com>
2023-08-28 20:22:28 -0600

    consumimos los datos de la api en nuestro componente

--- rickandmorty_api/src/Components/Character/Character.js
+++ rickandmorty_api/src/Components/Character/Character.js
@@ -1,8 +1,27 @@
 import React from 'react'
 
-const Character = () => {
+const Character = ({character}) => {
   return (
-    <div>Character</div>
+    <div className="container" style={{margin: "30px"}}>
+        <div className="row">
+            { 
+                character.map((item, index) => (
+                    <div key={index} className="col-lg-3 col-md-6 col-sm-12 mb-4">
+                        <div className="card" style={{minWidth: "250px"}}>
+                            <img className="card-img-top" src={item.image} alt="character"/>
+                            <div className="card-body">
+                                <h3 className="card-title">{item.name}</h3>
+                                <hr />
+                                <p className="card-text">Species: {item.species}</p>
+                                <p className="card-text">Gender: {item.gender}</p>
+                                <p className="card-text">Location: {item.location.name}</p>
+                                <p className="card-text">Status: {item.status}</p>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+        </div>
+    </div>
   )
 }
 
